172ae96b34e9806255e85c561f6d5e01ee73402d
trual <trual4@gmail.com>
2016-10-19 16:34:01 -0400

    Convert Roman numerals to Arabic classes setup

---
+++ RomanNumeralsKata/RomanNumeralsKata/RomanNumeralsToArabicTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanNumeralsKata
+{   
+    [TestClass()]
+    public class RomanNumeralsToArabicTest
+    {
+        RomanNumeralsToArabic ToArabic = new RomanNumeralsToArabic();
+
+        [TestMethod()]
+        public void convertIto1()
+        {
+            Assert.AreEqual("meow", ToArabic.converter("meow"));
+        }
+    }
+}
